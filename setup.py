c0a0333abcae6837e0a21698bfa0a75b27622aca
remosy <remosy>
2025-02-24 03:21:27 +0800

    up

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="codify",
+    name="codify4pp",
     version="0.1.0",
-    description="A tool to create project structures for deep learning projects.",
+    description="A tool to create project structures for deep learning paper publication.",
     author="Remosy",
     author_email="remosyxu@gmail.com",
     packages=find_packages(),
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "codify = codify.generate:main",
+            "codify4pp = codify4pp.generate:main",
         ],
     },
     classifiers=[
