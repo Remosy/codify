ff89f66d40ddf1c07268beed737e5b23037aa2c1
remosy <remosy>
2025-02-24 03:54:31 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codify4pp",
-    version="0.1.0",
+    version="0.1.1",
     description="A tool to create project structures for deep learning paper publication.",
     author="Remosy",
     author_email="remosyxu@gmail.com",
